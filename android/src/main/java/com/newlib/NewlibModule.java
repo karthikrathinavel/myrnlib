10a37a6824de86dbb077775a619bf639ccb6f364
Karthik Rathinavel <karthikrathinavel@outlook.com>
2023-01-23 11:17:46 +0530

    fix: minor typo in readme

--- android/src/main/java/com/newlib/NewlibModule.java
+++ android/src/main/java/com/newlib/NewlibModule.java
@@ -1,9 +1,14 @@
 package com.newlib;
 
-import android.content.Context;
-import android.widget.Toast;
 
+
+import android.content.Context;
+import android.media.AudioMetadata;
+import android.widget.Toast;
+import org.json.JSONObject;
 import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.ReactContext;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -13,16 +18,27 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
+
+import java.util.ArrayList;
+
+import io.mob.resu.reandroidsdk.IDeepLinkInterface;
+import io.mob.resu.reandroidsdk.MRegisterUser;
+import io.mob.resu.reandroidsdk.RNotification;
 import io.mob.resu.reandroidsdk.ReAndroidSDK;
 import io.mob.resu.reandroidsdk.ReReactNativeSDKPackage;
+
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
 @ReactModule(name = NewlibModule.NAME)
 public class NewlibModule extends ReactContextBaseJavaModule {
   public static final String NAME = "Newlib";
+  private final ReactApplicationContext reactContext;
   public Context context = null;
   public NewlibModule(ReactApplicationContext reactContext) {
 
-    super(reactContext);
-    context = (Context) reactContext;
+//    super(reactContext);
+//    context = (Context) reactContext;
+    this.reactContext = reactContext;
   }
 
   @Override
@@ -48,6 +64,7 @@ public class NewlibModule extends ReactContextBaseJavaModule {
   public void location(double latitude, double longitude){
     ReAndroidSDK.getInstance(context).onLocationUpdate(latitude, longitude);
     System.out.println("onLocationUpdate");
+    //emitEvent();
   }
 
   @ReactMethod
@@ -84,6 +101,77 @@ public class NewlibModule extends ReactContextBaseJavaModule {
   @ReactMethod
   public void onTrackEvent(){
     ReAndroidSDK.getInstance(context).onTrackEvent("SampleEvent");
+  }
+
+//  @ReactMethod
+//  public void onDeviceUserRegister(String uid, String name, String age, String email, String phone, String gender, String token, String profileUrl, String dob, String education, boolean employed, boolean married){
+//    MRegisterUser obj = new MRegisterUser();
+//    obj.setAdId(uid);
+//    obj.setName(name);
+//    obj.setAge(age);
+//    obj.setEmail(email);
+//    obj.setPhone(phone);
+//    obj.setGender(gender);
+//    obj.setDeviceToken(token);
+//    obj.setProfileUrl(profileUrl);
+//    obj.setDob(dob);
+//    obj.setEducation(education);
+//    obj.setEmployed(employed);
+//    obj.setMarried(married);
+//    ReAndroidSDK.getInstance(context).onDeviceUserRegister(obj);
+//  }
+
+  @ReactMethod
+  public void onDeviceUserRegister(String json) throws JSONException{
+    JSONObject json1 = new JSONObject(json);
+    System.out.println(json1);
+    MRegisterUser obj = new MRegisterUser();
+    obj.setAdId(json1.getString("uid"));
+    obj.setName(json1.getString("name"));
+    obj.setAge(json1.getString("age"));
+    obj.setEmail(json1.getString("email"));
+    obj.setPhone(json1.getString("phone"));
+    obj.setGender(json1.getString("gender"));
+    //obj.setDeviceToken(token);
+    obj.setProfileUrl(json1.getString("profileURL"));
+    obj.setDob(json1.getString("dob"));
+    obj.setEducation(json1.getString("education"));
+    obj.setEmployed(json1.getBoolean("employed"));
+    obj.setMarried(json1.getBoolean("married"));
+    ReAndroidSDK.getInstance(context).onDeviceUserRegister(obj);
+  }
+
+  @ReactMethod
+  public void formDataCapture(String json) throws JSONException {
+      JSONObject json1 = new JSONObject(json);
+//    json1.put("firstName", "John");
+//    json1.put("lastName", "Smith");
+//    json1.put("age", 25);
+    ReAndroidSDK.getInstance(context).formDataCapture(json1);
+  }
+
+    @ReactMethod
+    public void readNotification(){
+      ReAndroidSDK.getInstance(context).readNotification("1673591227344");
+    }
+
+  @ReactMethod
+  public void unReadNotification(){
+    ReAndroidSDK.getInstance(context).unReadNotification("1673591227344");
+  }
+
+  @ReactMethod
+  public void getDeepLinkData(){
+    String deepLinkData = ReAndroidSDK.getInstance(context).getDeepLinkData();
+    System.out.println(deepLinkData);
+  }
+
+  @ReactMethod
+  public void emitEvent() {
+    String data = "Data";
+    reactContext
+      .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+      .emit("Sample Event", data);
   }
 
 }
--- example/src/App.js
+++ example/src/App.js
@@ -1,95 +1,156 @@
-import React, {useEffect} from 'react';
-import {Text, TouchableOpacity, View, StyleSheet, NativeModules} from 'react-native';
-import {requestUserPermission, NotificationServices} from './notif/utils/PushNotifications.js';
-import Newlib from './CustomModule';
-const App = () => {
-  useEffect(()=>{
-    requestUserPermission();
-    NotificationServices();
-  },[]);
-  function callme(){
-     Newlib.toast();
-  }
-  function locationUpdate(){
-      var location = {
-       latitude: 13.067439,
-       longitude: 80.237617
-      }
-      Newlib.location(location.latitude, location.longitude);
-    }
-  function conversion(){
-      var data = '{"name":"John", "age":30, "city":"New York"}';
-      Newlib.appConversionTracking(data);
-    }
-  function addNewNotification(){
-      var notificationTitle = "Sample Title";
-      var notificationBody = "Sample Body";
-      Newlib.addNewNotification(notificationTitle, notificationBody);
-    }
-    function delNotificationById(){
-      var id = "1673501625026";
-      Newlib.deleteNotificationByNID(id);
-    }
-    function getReadNotificationCount(){
-      Newlib.getReadNotificationCount();
-    }
-    function customEvent1(){
-      var resEvent = {
-        eventName: 'Product Purchased',
-        data: {
-          productId: 'P234234',
-          productName: 'Mobile Phone'
-        }
-      };
-      NativeModules.ReReactNativeSDK.customEvent(JSON.stringify(resEvent));
-    }
-    function updatePushToken(){
-      Newlib.updatePushToken();
-    }
-    function onTrackEvent(){
-      Newlib.onTrackEvent();
-    }
-    function onDeviceUserRegister(){
-      Newlib.onDeviceUserRegister();
-    }
-  return(
-  <View>
-    <TouchableOpacity onPress={locationUpdate} style={styles.countContainer}>
-      <Text style={{color: '#fff'}}>locationUpdate</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={conversion} style={styles.countContainer}>
-          <Text style={{color: '#fff'}}>appConversionTracking</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={addNewNotification} style={styles.countContainer}>
-          <Text style={{color: '#fff'}}>getNotification</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={delNotificationById} style={styles.countContainer}>
-          <Text style={{color: '#fff'}}>DelNotificationbyid</Text>
-          </TouchableOpacity>
-        <TouchableOpacity onPress={getReadNotificationCount} style={styles.countContainer}>
-          <Text style={{color: '#fff'}}>getReadNotificationCount</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={customEvent1} style={styles.countContainer}>
-          <Text style={{color: '#fff'}}>customEvent</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={updatePushToken} style={styles.countContainer}>
-          <Text style={{color: '#fff'}}>updatePushToken</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={onTrackEvent} style={styles.countContainer}>
-          <Text style={{color: '#fff'}}>onTrackEvent</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={onTrackEvent} style={styles.countContainer}>
-          <Text style={{color: '#fff'}}>onDeviceUserRegister</Text>
-        </TouchableOpacity>
-  </View>
-  );
-}
-const styles = StyleSheet.create({
-  countContainer: {
-    alignItems: "center",
-    padding: 10,
-    margin: 20,
-    backgroundColor: 'teal',
-  }
-});
-export default App;
+//import React from 'react';
+//import {Text, TouchableOpacity, ScrollView, StyleSheet, NativeModules, Button, DeviceEventEmitter} from 'react-native';
+//import {requestUserPermission, NotificationServices} from './notif/utils/PushNotifications.js';
+//import Newlib from './CustomModule';
+//const App = () => {
+//  const [name, setName] = useState('');
+//  const [email, setEmail] = useState('');
+//  useEffect(()=>{
+//    requestUserPermission();
+//    NotificationServices();
+//    setTimeout(()=>{
+//      Newlib.emitEvent();
+//    },3000)
+//  },[]);
+//  DeviceEventEmitter.addListener('Sample Event', (data) => {
+//      console.log(data);
+//  });
+//  function callme(){
+//     Newlib.toast();
+//  }
+//  function locationUpdate(){
+//      var location = {
+//       latitude: 13.067439,
+//       longitude: 80.237617
+//      }
+//      Newlib.location(location.latitude, location.longitude);
+//    }
+//  function conversion(){
+//      var data = '{"name":"John", "age":30, "city":"New York"}';
+//      Newlib.appConversionTracking(data);
+//    }
+//  function addNewNotification(){
+//      var notificationTitle = "Sample Title";
+//      var notificationBody = "Sample Body";
+//      Newlib.addNewNotification(notificationTitle, notificationBody);
+//    }
+//    function delNotificationById(){
+//      var id = "1673501625026";
+//      Newlib.deleteNotificationByNID(id);
+//    }
+//    function getReadNotificationCount(){
+//      Newlib.getReadNotificationCount();
+//    }
+//    function customEvent1(){
+//      var resEvent = {
+//        eventName: 'Product Purchased',
+//        data: {
+//          productId: 'P234234',
+//          productName: 'Mobile Phone'
+//        }
+//      };
+//      NativeModules.ReReactNativeSDK.customEvent(JSON.stringify(resEvent));
+//    }
+//    function updatePushToken(){
+//      Newlib.updatePushToken();
+//    }
+//    function onTrackEvent(){
+//      Newlib.onTrackEvent();
+//    }
+//    function onDeviceUserRegister(){
+//      var uid = "1", name = "karthik", age = "22", email = "email@email.com", phone = "1234567890", gender = "M", token = "ddkO8qkqSty2R3shWWXjaX:APA91bFWIrDtEREcVcz_TJpMbgI4a4tj3yvAOgS6-hSC-zlrThjJohnyYIkMC_Y6xyREV0kmOabrMK3EZklPkQSvYqDVOl5ZNoc33huEEhmL7Anwqs7zuIHWpjYl11sCAbJT6zDiASFA";
+//      var profileUrl = "https://github.com/karthikrathinavel", dob = "26/01/2001", education = "B.E";
+//      var employed = false, married = false;
+//      let jsonData = {
+//        "uid": "1",
+//        "name": "karthik",
+//        "age": "22",
+//        "email": "email2@email.com",
+//        "phone": "1234567890",
+//        "gender": "M",
+//        "profileURL": "https://github.com/karthikrathinavel",
+//        "dob": "26/01/2001",
+//        "education": "B.E",
+//        "employed": false,
+//        "married": false
+//      }
+//      let jsonString = JSON.stringify(jsonData);
+//      Newlib.onDeviceUserRegister(jsonString);
+//    }
+//    function readNotification(){
+//      Newlib.readNotification();
+//    }
+//    function unReadNotification(){
+//      Newlib.unReadNotification();
+//    }
+//    function getDeepLinkData(){
+//      Newlib.getDeepLinkData();
+//    }
+//    function formDataCapture(){
+//      let jsonData = {
+//          "name": "John",
+//          "age": 30,
+//          "city": "New York"
+//      };
+//      let jsonString = JSON.stringify(jsonData);
+//      Newlib.formDataCapture(jsonString);
+//    }
+//    function emitEvent(){
+//      Newlib.emitEvent();
+//    }
+//  return(
+//  <ScrollView>
+//    <TouchableOpacity onPress={locationUpdate} style={styles.countContainer}>
+//      <Text style={{color: '#fff'}}>locationUpdate</Text>
+//    </TouchableOpacity>
+//    <TouchableOpacity onPress={conversion} style={styles.countContainer}>
+//          <Text style={{color: '#fff'}}>appConversionTracking</Text>
+//        </TouchableOpacity>
+//        <TouchableOpacity onPress={addNewNotification} style={styles.countContainer}>
+//          <Text style={{color: '#fff'}}>getNotification</Text>
+//        </TouchableOpacity>
+//        <TouchableOpacity onPress={delNotificationById} style={styles.countContainer}>
+//          <Text style={{color: '#fff'}}>DelNotificationbyid</Text>
+//          </TouchableOpacity>
+//        <TouchableOpacity onPress={getReadNotificationCount} style={styles.countContainer}>
+//          <Text style={{color: '#fff'}}>getReadNotificationCount</Text>
+//        </TouchableOpacity>
+//        <TouchableOpacity onPress={customEvent1} style={styles.countContainer}>
+//          <Text style={{color: '#fff'}}>customEvent</Text>
+//        </TouchableOpacity>
+//        <TouchableOpacity onPress={updatePushToken} style={styles.countContainer}>
+//          <Text style={{color: '#fff'}}>updatePushToken</Text>
+//        </TouchableOpacity>
+//        <TouchableOpacity onPress={onTrackEvent} style={styles.countContainer}>
+//          <Text style={{color: '#fff'}}>onTrackEvent</Text>
+//        </TouchableOpacity>
+//        <TouchableOpacity onPress={onDeviceUserRegister} style={styles.countContainer}>
+//          <Text style={{color: '#fff'}}>onDeviceUserRegister</Text>
+//        </TouchableOpacity>
+//        <TouchableOpacity onPress={readNotification} style={styles.countContainer}>
+//          <Text style={{color: '#fff'}}>readNotification</Text>
+//        </TouchableOpacity>
+//        <TouchableOpacity onPress={unReadNotification} style={styles.countContainer}>
+//          <Text style={{color: '#fff'}}>unReadNotification</Text>
+//        </TouchableOpacity>
+//        <TouchableOpacity onPress={getDeepLinkData} style={styles.countContainer}>
+//          <Text style={{color: '#fff'}}>getDeepLinkData</Text>
+//        </TouchableOpacity>
+//        <TouchableOpacity onPress={formDataCapture} style={styles.countContainer}>
+//          <Text style={{color: '#fff'}}>formDataCapture</Text>
+//        </TouchableOpacity>
+//        <TouchableOpacity onPress={emitEvent} style={styles.countContainer}>
+//                  <Text style={{color: '#fff'}}>EmitEvent</Text>
+//                </TouchableOpacity>
+//  </ScrollView>
+//  );
+//}
+//const styles = StyleSheet.create({
+//  countContainer: {
+//    alignItems: "center",
+//    padding: 10,
+//    margin: 5,
+//    backgroundColor: 'rgba(0,0,0,0.2)',
+//  }
+//});
+//export default App;
