08667246192edde57c547f816cba9d0bbf4ce52b
Karthik Rathinavel <80736284+karthikrathinavel@users.noreply.github.com>
2023-01-23 11:25:32 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,32 @@
-// import { NativeModules, Platform, NativeEventEmitter } from 'react-native';
-//
-// const LINKING_ERROR =
-//   `The package 'react-native-newlib' doesn't seem to be linked. Make sure: \n\n` +
-//   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-//   '- You rebuilt the app after installing the package\n' +
-//   '- You are not using Expo Go\n';
-//
-// const Newlib = NativeModules.Newlib
-//   ? NativeModules.Newlib
-//   : new Proxy(
-//       {},
-//       {
-//         get() {
-//           throw new Error(LINKING_ERROR);
-//         },
-//       }
-//     );
-//
-// export function multiply(a: number, b: number): Promise<number> {
-//   return Newlib.multiply(a, b);
-// }
-// export function locationUpdate(lat: number, long: number){
-//       //return Newlib.location(lat, long);
-//       console.log("LocationUpdate");
-//       Newlib.location(lat,long);
-// }
-// const eventEmitter = new NativeEventEmitter(NativeModules.Newlib);
-//
-// eventEmitter.addListener('Sample Event', (event: any) => {
-//   console.log(event);
-// });
+import { NativeModules, Platform, NativeEventEmitter } from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'react-native-newlib' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+const Newlib = NativeModules.Newlib
+  ? NativeModules.Newlib
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export function multiply(a: number, b: number): Promise<number> {
+  return Newlib.multiply(a, b);
+}
+export function locationUpdate(lat: number, long: number){
+      //return Newlib.location(lat, long);
+      console.log("LocationUpdate");
+      Newlib.location(lat,long);
+}
+const eventEmitter = new NativeEventEmitter(NativeModules.Newlib);
+
+eventEmitter.addListener('Sample Event', (event: any) => {
+  console.log(event);
+});
